4f1d01724516fa4c3b03eae01fc5863a21beeaf1
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-09-17 16:53:41 -0700

    fix images and add state to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import _ from 'lodash';
 import * as d3 from "d3";
 
@@ -8,11 +8,18 @@ import charList from './data/char_list.json';
 import characters from './data/characters.json';
 import lines from './data/lines.json';
 
-var width = 800;
-var height = 800;
-// var color = d3.scaleOrdinal(d3.schemeCategory20);
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 var radius = 3;
-class App extends Component {
+var App = React.createClass({
+
+  getInitialState() {
+    return {
+      width: 800,
+      height: 800,
+      linesByCharacter: [],
+      characterPositions: [],
+    };
+  },
 
   componentWillMount() {
     // get the length of each set of lines
@@ -25,7 +32,7 @@ class App extends Component {
     	.range([radius, radius * 5]);
 
     // now duplicate any of the lines sung by multiple characters
-    this.linesByCharacter = _.chain(lines)
+    var linesByCharacter = _.chain(lines)
       .map((line, id) => {
         // get all characters from the line
         return _.map(line[1][0], (character) => {
@@ -34,32 +41,45 @@ class App extends Component {
             lineId: id,
             characterId: character,
             radius: radiusScale(line[3]),
-            x: width / 2,
-            y: height / 2,
+            x: this.state.width / 2,
+            y: this.state.height / 2,
+            color: color(character),
             data: line,
           };
         });
       }).flatten().value();
 
     // get only the top 12 individuals by line count
-    this.topChars = _.chain(characters.characters)
+    var topChars = _.chain(characters.characters)
       .map((lines, character) => [character, lines.length])
       // only keep individual characters' lines
       .filter((character) => charList[character[0]][2] === 'individual')
       .sortBy((character) => -character[1])
-      .map(0).take(12).value();
-    this.topChars.push('other');
+      .map(0)
+      .take(12)
+      .value();
+    topChars.push('other');
 
     // now position the characters
     var perRow = 4;
-    var rowWidth = width / (perRow + 1);
-    this.charPos = _.reduce(this.topChars, (obj, character, i) => {
+    var rowWidth = this.state.width / perRow;
+    var characterPositions = _.reduce(topChars, (obj, character, i) => {
+      // if it's the first two, give them more room
+      var fx = this.state.width / 3 * (i + 1);
+      var fy = this.state.width / 3 * .5;
+      if (i >= 2) {
+        i -= 2;
+        fx = (i % perRow + .5) * rowWidth;
+        fy = (Math.floor(i / perRow) + .5) * (rowWidth);
+        fy += this.state.width / 3; // offset the top two
+      }
       obj[character] = {
         id: character,
         name: charList[character] ? charList[character][0] : 'Other',
-        x: (i % perRow + .5) * rowWidth,
-        y: (Math.floor(i / perRow) + .5) * rowWidth,
-        radius: radius * 10,
+        fx,
+        fy,
+        radius: 20,
+        color: color(character),
       };
       if (character !== 'other') {
         // only load image if it's a character
@@ -69,20 +89,27 @@ class App extends Component {
     }, {});
 
     // now assign the character positions to all nodes
-    _.each(this.linesByCharacter, line => {
-      var pos = this.charPos[line.characterId] || this.charPos['other'];
-      line.focusX = pos.x;
-      line.focusY = pos.y;
+    _.each(linesByCharacter, line => {
+      var pos = characterPositions[line.characterId] ||
+        characterPositions['other'];
+      line.focusX = pos.fx;
+      line.focusY = pos.fy;
     });
-  }
+
+    // now that we've set the positions, take out "other"
+    delete characterPositions['other'];
+    characterPositions = _.values(characterPositions);
+
+    this.setState({linesByCharacter, characterPositions});
+  },
 
   render() {
     return (
       <div className="App">
-        <Visualization />
+        <Visualization {...this.state} />
       </div>
     );
   }
-}
+});
 
 export default App;
