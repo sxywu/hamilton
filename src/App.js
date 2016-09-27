3318bf37fe932e42857ba0720a407f4ea6044743
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-09-27 16:22:50 -0700

    move data processing to ProcessGraph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import _ from 'lodash';
-import * as d3 from "d3";
 
 import Visualization from './Visualization';
 import Characters from './Characters';
 import ProcessGraph from './ProcessGraph';
-// load the data
-import charList from './data/char_list.json';
-import songList from './data/song_list.json';
-import themeList from './data/theme_list.json';
-import rawCharacters from './data/characters.json';
-import rawLines from './data/lines.json';
-import rawThemes from './data/themes.json';
-// positions
-import charPositions from './data/char_positions.json';
-// import lineCharPositions from './data/line_char_positions.json';
-// import lineSongPositions from './data/line_song_positions.json';
-
-var color = d3.scaleOrdinal(d3.schemeCategory20);
-var linkScale = d3.scaleLinear().range([2, 8]);
 
 var App = React.createClass({
 
@@ -38,119 +23,11 @@ var App = React.createClass({
   },
 
   componentWillMount() {
-    // duplicate any of the lines sung by multiple characters
-    var lines = _.chain(rawLines)
-      .map((line, lineId) => {
-        // get all characters from the line
-        return _.map(line[1][0], (character, i) => {
-          var id = character + '/' + lineId;
-          var songId = lineId.split(':')[0];
+    var {lines, songs} = ProcessGraph.processLinesSongs();
 
-        	return {
-            id,
-            lineId,
-            songId,
-            characterId: character,
-            characterName: charList[character][0],
-            songName: songList[songId],
-            numSingers: line[1][0].length,
-            singerIndex: i,
-            conversing: null,
-            fill: color(character),
-            trueFill: color(character),
-            selected: true,
-            data: line,
-          };
-        });
-      }).flatten().value();
-    var linesById = _.groupBy(lines, 'lineId');
+    var {characterNodes, characterLinks} = ProcessGraph.processCharacters(lines);
 
-    // character nodes
-    var characterNodes = _.map(rawCharacters.characters, (lines, id) => {
-      var character = charList[id];
-      var name = character[0];
-      var initials = _.map(name.split(' '), 0).join('');
-
-      return {
-        id,
-        name,
-        initials,
-        radius: 20,
-        color: color(id),
-        image: character[3] && require('./images/' + id + '.png'),
-        selected: true,
-      };
-    });
-    var charactersById = _.keyBy(characterNodes, 'id');
-
-    // character links
-    var conversingValues = _.values(rawCharacters.conversing);
-    var minWidth = _.minBy(conversingValues, (lines) => lines.length).length;
-    var maxWidth = _.maxBy(conversingValues, (lines) => lines.length).length;
-    linkScale.domain([minWidth, maxWidth]);
-    var characterLinks = _.map(rawCharacters.conversing, (lines, conversing) => {
-      var source = conversing.split('-');
-      var target = charactersById[source[1]];
-      source = charactersById[source[0]];
-      var weight = linkScale(lines.length);
-
-      _.each(lines, lineId => {
-        // for each line that has this conversation,
-        // there could be multiple characters, so go through them all
-        _.each(linesById[lineId], line => {
-          if (line.characterId === source.id) {
-            line.conversing = conversing;
-          }
-        });
-      });
-
-      return {
-        id: conversing,
-        color: source.color,
-        source, target, weight
-      };
-    });
-
-    var diamonds = _.chain(rawThemes)
-      .map((lineKeys, theme) => {
-        if (!themeList[theme][2]) return null;
-
-        return _.map(lineKeys, (lineKey) => {
-          var lineId = lineKey[0][0];
-          var songId = parseInt(lineId.split(':')[0], 10);
-          var startLine = lineId.split(':')[1].split('/');
-          var startLineId = songId + ':' + startLine[1];
-          startLine = parseInt(startLine[0], 10);
-          var endLine = _.last(lineKey[0]).split(':')[1].split('/');
-          var endLineId = songId + ':' + endLine[1];
-          endLine = parseInt(endLine[0], 10);
-
-          return {
-            id: theme + '/' + songId + ':' + startLine,
-            themeId: theme,
-            themeType: themeList[theme][1],
-            themeLines: themeList[theme][0],
-            lineId: lineId.split('/')[0],
-            songId,
-            startLine,
-            endLine,
-            startLineId,
-            endLineId,
-            fill: color(theme),
-            keys: lineKey[0],
-            lines: lineKey[1],
-          }
-        });
-      }).filter().flatten()
-      .value();
-
-    var songs = _.reduce(songList, (obj, name, id) => {
-      obj[id] = {
-        id,
-        name,
-      }
-      return obj;
-    }, {});
+    var {diamonds} = ProcessGraph.processThemes();
 
     this.filterAndPosition(this.state.selectedCharacters, this.state.selectedConversation,
       characterNodes, characterLinks, lines, diamonds, songs);
