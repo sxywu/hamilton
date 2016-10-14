8e01eef335094e107a88d4e83bad89fb186a27c5
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-10-14 12:56:17 -0700

    color music lines

--- src/Section.js
+++ src/Section.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import * as d3 from "d3";
+import charList from './data/char_list.json';
+import chroma from 'chroma-js';
 var Remarkable = require('remarkable');
 var md = new Remarkable({linkTarget: '_new', html: true});
 
 var Section = React.createClass({
 
+  componentDidMount() {
+    // use this section to post-process for music
+    var color = this.props.fontColor;
+    d3.select(this.refs.section)
+      .selectAll('.music')
+      .each(function() {
+        var character = d3.select(this).attr('data-char');
+        var background = charList[character][4];
+        if (chroma.contrast(background, color) < 4.5) {
+          // if the contrast isn't enough, make the color white isntead
+          color = '#fff';
+        }
+        d3.select(this).selectAll('span')
+          .style('background', background)
+          .style('color', color)
+          .style('padding', '0 6px')
+          .style('font-weight', 400);
+      });
+  },
+
   render() {
     var style = {
       width: this.props.width,
-      paddingTop: '40vh',
-      paddingBottom: '25vh',
+      marginTop: '40vh',
+      marginBottom: '25vh',
       fontSize: 16,
       lineHeight: 1.6,
       paddingLeft: 20,
       marginLeft: this.props.vizAlign === 'left' ? this.props.left : 0,
       pointerEvents: 'auto',
+      color: this.props.fontColor,
+      backgroundColor: 'rgba(255, 255, 255, 0.75)',
     };
     var rawMarkup = { __html: md.render(this.props.text)};
 
     return (
-      <div style={style} className='section' id={this.props.id}
+      <div style={style} ref='section' className='section'
+        id={this.props.id}
         dangerouslySetInnerHTML={rawMarkup} />
     );
   }
--- src/data/sections.js
+++ src/data/sections.js
@@ -6,11 +6,13 @@ var sections = [
     text: `
 I first heard Hamilton while visiting my friend in St. Louis for her birthday.  For that entire weekend, I heard the same songs from the soundtrack on repeat.  She kept humming the same lines over and over:
 
-*Oui oui, mon ami, je m’apelle Lafeyette!<br />
-The Lancelot of the Revolutionary set!<br />
-I came from afar, just to say “Bonsoir!”<br />
-Tell the King, “Casse toi!” Who’s the best?<br />
-C’est moi!*
+<div class='music' data-char='5'>
+  <span>Oui oui, mon ami, je m’apelle Lafeyette!</span><br />
+  <span>The Lancelot of the Revolutionary set!</span><br />
+  <span>I came from afar, just to say “Bonsoir!”</span><br />
+  <span>Tell the King, “Casse toi!” Who’s the best?</span><br />
+  <span>C’est moi!</span>
+</div>
 
 Catchy as it was, I hated the song by day two, and was ready to throttle her by day three.  Needless to say, I flew back to San Francisco cursing Hamilton.
 
@@ -25,18 +27,20 @@ After that glowing review, I knew I had to at least give it a try.
     text: `
 The first few times I listened, I accidentally had the soundtrack on shuffle.  I was tuning in and out of the lyrics when I suddenly heard:
 
-*When you’re gone, I’ll go mad<br />
-So don’t throw away this thing we had<br />
-Cuz when push comes to shove<br />
-I will kill your friends and family to remind you of my love*
+<div class='music' data-char='12'>
+  <span>When you’re gone, I’ll go mad</span><br />
+  <span>So don’t throw away this thing we had</span><br />
+  <span>Cuz when push comes to shove</span><br />
+  <span>I will kill your friends and family to remind you of my love</span>
+</div>
 
-And I was like, holy crap what is this super abusive relationship?!  To which my boyfriend had to reassure me that it was just King George singing to America (does that make things better?  I’m still not sure).  After that, I decided to actually listen to all the lyrics the whole way through.
+And I was like, holy crap what is this super abusive relationship?!  To which my boyfriend had to clarify that it was just King George singing to America (does that make things better?  I’m still not sure).  After that, I decided to actually listen to all the lyrics the whole way through.
 
-The first time, I was sobbing by the end.  It was that f*cking transformative.
+That first time, I was sobbing by the end.
 
-I had it on repeat for months.  It was all I listened to in my waking hours.  I was obsessed.  My boyfriend and I listened so much we started to analyze the lyrics and musical styles.  We reveled in the layers of complexity, the double entredres, the clever word plays.
+I had it on repeat for months.  It was all I listened to in my waking hours.  I was obsessed.  We listened so much we started to analyze the lyrics and musical styles.  We reveled in the layers of complexity, the double entredres, the clever word plays.
 
-We started to wonder what a visualization of Hamilton would look like.
+### We started to wonder what a visualization of Hamilton would look like.
     `
   }
 ];
