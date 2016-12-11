2efdf22f4517a451884d486408f4b3dac302f069
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-12-10 17:23:03 -0800

    refactor themes to use d3, add hover

---
+++ src/Hover.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import _ from 'lodash';
+import * as d3 from "d3";
+
+var borderRadius = 3;
+var gray = '#aaa';
+
+var Hover = React.createClass({
+
+  render() {
+    if (_.isEmpty(this.props.hovered)) {
+      return (<div></div>);
+    }
+
+    var hovered = this.props.hovered;
+    var style = {
+      position: 'absolute',
+      backgroundColor: '#fff',
+      border: '1px solid ' + gray,
+      borderRadius,
+      boxShadow: '0 0 5px ' + gray,
+      textAlign: 'left',
+      fontSize: 12,
+      lineHeight: 1.5,
+      width: hovered.width,
+      left: hovered.left - hovered.width / 2,
+      top: hovered.top,
+      padding: 10,
+    };
+    var metaStyle = {
+      fontSize: 9,
+      color: this.props.medGray,
+      verticalAlign: 'middle',
+      fontStyle: 'italic',
+      textAlign: 'right',
+      padding: '0 5px',
+    };
+
+    var lines = _.map(hovered.lines, (line, i) => {
+      return (
+        <tr>
+          <td style={metaStyle}>{i + 1}</td>
+          <td>{line}</td>
+        </tr>
+      );
+    });
+    return (
+      <div ref='summary' style={style}>
+        <table>
+          <tbody>
+            {lines}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
+
+export default Hover;
--- src/visualizations/Themes.js
+++ src/visualizations/Themes.js
@@ -3,14 +3,13 @@ import _ from 'lodash';
 import * as d3 from 'd3';
 
 import Diamonds from './Diamonds';
-import LineHover from '../LineHover';
+import Hover from '../Hover';
 
 var fontSize = 12;
 var Themes = React.createClass({
   getInitialState() {
     return {
-      update: true,
-      sideHovered: null,
+      hovered: null,
     };
   },
 
@@ -18,13 +17,100 @@ var Themes = React.createClass({
     return nextProps.update;
   },
 
-  hoverSideTheme(hoveredTheme) {
-    var sideHovered = hoveredTheme && {
-      lines: hoveredTheme.lines,
-      x: d3.event.x,
-      y: d3.event.y,
+  componentDidMount() {
+    this.container = d3.select(this.refs.container);
+
+    this.updateThemes();
+  },
+
+  componentDidUpdate() {
+    this.updateThemes();
+  },
+
+  updateThemes() {
+    // only show grouped themes that aren't filtered out
+    // var groupedThemes = _.filter(this.props.groupedThemes,
+    //   theme => _.some(theme.diamonds, diamond => diamond.available));
+    var groupedThemes = this.props.groupedThemes;
+    this.themes = this.container.selectAll('h3')
+      .data(groupedThemes, d => d.name);
+
+    this.themes.exit().remove();
+
+    var enter = this.themes.enter().append('h3')
+      .style('display', 'inline-block')
+      .style('padding', '5px 10px')
+      .style('margin', 0);
+
+    // write theme name
+    enter.append('div')
+      .text(d => d.name);
+
+    // svg for the diamonds
+    enter.append('svg')
+      .attr('width', d => d.width)
+      .attr('height', fontSize * 2);
+
+    this.themes = enter.merge(this.themes);
+    var svgs = this.themes.select('svg');
+
+    // diamond groups
+    var diamonds = svgs.selectAll('g')
+      .data(d => d.diamonds, d => d.id);
+
+    diamonds.exit().remove();
+
+    // enter path and text
+    enter = diamonds.enter().append('g');
+    enter.append('path')
+      .attr('fill', 'none')
+      .attr('stroke', this.props.fontColor);
+    enter.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('font-family', this.props.bodyFont)
+      .attr('font-style', 'italic')
+      .attr('font-size', fontSize);
+    enter.append('rect')
+      .attr('y', -fontSize)
+      .attr('height', fontSize)
+      .style('opacity', 0);
+
+    // update path and text for diamonds
+    diamonds = enter.merge(diamonds)
+      .attr('transform', d => 'translate(' + [d.x, fontSize] + ')')
+      .attr('opacity', d => d.available ? (d.selected ? 1 : 0.5) : 0)
+      .attr('cursor', d => d.available ? 'pointer' : 'default');
+
+    diamonds.select('rect')
+      .attr('width', d => d.x2 + fontSize / 2)
+      .on('click', d => d.available ? this.props.onSelectTheme(d.id) : null)
+      .on('mouseover', d => d.available ? this.mouseOver(d) : null)
+      .on('mouseleave', d => this.mouseOver(null));
+
+    diamonds.select('path')
+      .attr('d', this.calculateCurve);
+
+    diamonds.select('text')
+      .attr('x', d => d.x2)
+      .text(d => d.themeType[0].toLowerCase() + d.groupId);
+  },
+
+  mouseOver(diamond) {
+    if ((diamond && this.state.hovered && diamond.id === this.state.hovered.id) ||
+      (!diamond && !this.state.hovered)) return;
+
+    var containerRect = this.refs.container.getBoundingClientRect();
+    var diamondRect = d3.event.target.getBoundingClientRect();
+    var top = (diamondRect.top - containerRect.top) + diamondRect.height + 5;
+    var left = (diamondRect.left - containerRect.left) + diamondRect.width / 2;
+
+    var hovered = diamond && {
+      lines: diamond.lines,
+      width: 200,
+      top,
+      left,
     }
-    this.setState({sideHovered, update: false});
+    this.setState({hovered});
   },
 
   calculateCurve(diamond) {
@@ -42,56 +128,13 @@ var Themes = React.createClass({
       height: this.props.themeHeight,
       verticalAlign: 'top',
       textAlign: 'center',
+      position: 'relative',
     };
-    var groupStyle = {
-      display: 'inline-block',
-      padding: '5px 10px',
-      margin: 0,
-    };
-
-    var themes = _.map(this.props.groupedThemes, theme => {
-      var diamonds = _.map(theme.diamonds, diamond => {
-        var name = diamond.themeType[0].toLowerCase() + diamond.groupId;
-        var gAttr = {
-          transform: 'translate(' + [diamond.x, fontSize] + ')',
-          opacity: diamond.available ? (diamond.selected ? 1 : 0.5) : 0,
-          cursor: diamond.available ? 'pointer' : 'default',
-        };
-        var gClick = diamond.available ? this.props.onSelectTheme.bind(null, diamond.id) : null;
-        var pathAttr = {
-          d: this.calculateCurve(diamond),
-          stroke: this.props.fontColor,
-          fill: 'none',
-        };
-        var textAttr = {
-          x: diamond.x2,
-          textAnchor: 'middle',
-          fontFamily: this.props.bodyFont,
-          fontStyle: 'italic',
-          fontSize,
-        };
-        return (
-          <g {...gAttr} onClick={gClick}>
-            <path {...pathAttr} />
-            <text {...textAttr}>{name}</text>
-          </g>
-        );
-      });
-
-      return (
-        <h3 style={groupStyle}>
-          {theme.name}<br />
-          <svg width={theme.width} height={fontSize * 2}>
-            {diamonds}
-          </svg>
-        </h3>
-      );
-    });
 
     return (
       <div style={style}>
-        {themes}
-        <LineHover {...this.state.sideHovered} />
+        <div ref='container' />
+        <Hover {...this.props} {...this.state} />
       </div>
     );
   }
