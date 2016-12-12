003e223a69617e8473c120a3954466d1ff36ea83
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-12-12 13:23:25 -0800

    start header and footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './header.css';
+import './footer.css';
 
 ReactDOM.render(
   <App />,
